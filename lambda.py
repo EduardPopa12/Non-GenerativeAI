5a5b0aa6e1f71c418d0b1137076aab694d36ef1a
Popa Eduard <72855107+EduardPopa12@users.noreply.github.com>
2023-08-18 12:46:01 +0300

    Add files via upload
    There is no requirements,txt file for the lambda script. The lambda script can perform its functionality on AWS without the need to install any additional libraries (including boto3)

---
+++ lambda.py
@@ -0,0 +1,44 @@
+import boto3
+import json
+import os
+
+def lambda_handler(event, context):
+    # Initialize AWS clients
+    ec2_client = boto3.client('ec2')
+    s3_client = boto3.client('s3')
+
+    # Collect metrics
+    unattached_volumes = []
+    not_encrypted_volumes = []
+    not_encrypted_snapshots = []
+
+    # Describe all volumes
+    volumes_response = ec2_client.describe_volumes()
+    for volume in volumes_response['Volumes']:
+        if 'Attachments' not in volume:
+            unattached_volumes.append(volume['VolumeId'])
+            if not volume.get('Encrypted'):
+                not_encrypted_volumes.append(volume['VolumeId'])
+
+    # Describe all snapshots
+    snapshots_response = ec2_client.describe_snapshots(OwnerIds=['self'])
+    for snapshot in snapshots_response['Snapshots']:
+        if not snapshot.get('Encrypted'):
+            not_encrypted_snapshots.append(snapshot['SnapshotId'])
+
+    # Store metrics in S3
+    metrics = {
+        'unattached_volumes': unattached_volumes,
+        'not_encrypted_volumes': not_encrypted_volumes,
+        'not_encrypted_snapshots': not_encrypted_snapshots
+    }
+
+    # Store metrics in S3
+    s3_bucket = os.environ['S3_BUCKET_NAME']
+    s3_key = 'metrics.json'
+    
+    s3_client.put_object(
+        Bucket=s3_bucket,
+        Key=s3_key,
+        Body=json.dumps(metrics)
+    )
